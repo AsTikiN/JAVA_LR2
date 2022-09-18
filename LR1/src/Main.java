d401e5c8bbf85fa746ecab2c4b743834728828d7
AsTikiN <77283605+AsTikiN@users.noreply.github.com>
2022-09-18 15:13:03 +0300

    Update Main.java

--- LR1/src/Main.java
+++ LR1/src/Main.java
@@ -2,6 +2,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        /* changes from browser */
         System.out.println("Hello world!");
         System.out.println("This task done by Nikita Sivtsov SE-19-1/9");
         Date date = new Date();
