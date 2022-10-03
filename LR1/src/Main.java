94a91c09ca97f2d6a7757260fedc867769580f82
AsTikiN <77283605+AsTikiN@users.noreply.github.com>
2022-10-03 10:52:15 +0300

    remove updates

--- LR1/src/Main.java
+++ LR1/src/Main.java
@@ -4,7 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This task done by Nikita Sivtsov SE-19-1/9");
-        Date date = new Date();
-        System.out.println("Time now :" +date);
     }
 }
